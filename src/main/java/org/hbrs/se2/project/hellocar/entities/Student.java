5f60a822b84729b707df9b8747b13c156f06d44c
sara <sara.adni@smail.inf.h-brs.de>
2025-05-11 22:54:28 +0200

    Erstellung Entitätsklasse Student

--- src/main/java/org/hbrs/se2/project/hellocar/entities/Student.java
+++ src/main/java/org/hbrs/se2/project/hellocar/entities/Student.java
@@ -1,4 +1,16 @@
 package org.hbrs.se2.project.hellocar.entities;
 
+
+
+import jakarta.persistence.*;
+import lombok.Setter;
+import lombok.Getter;
+
+@Setter
+@Getter
+@Table(name = "student", schema = "public")
 public class Student {
+    @Id
+
 }
+
